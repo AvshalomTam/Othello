facfda04aee7686a808b90a4b1aa87fc17b2a996
Avshalom <avshalom.tam@gmail.com>
2017-11-22 23:36:11 +0200

    added decisions.docx file describing oop and other consideration while working on shared project.

--- cmake-build-debug/CMakeFiles/Othello.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Othello.dir/DependInfo.cmake
@@ -4,16 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/steve/Desktop/Othello/src/AIplayer.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/AIplayer.cpp.o"
-  "/home/steve/Desktop/Othello/src/BasicRules.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/BasicRules.cpp.o"
-  "/home/steve/Desktop/Othello/src/Board.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Board.cpp.o"
-  "/home/steve/Desktop/Othello/src/CharBoard.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/CharBoard.cpp.o"
-  "/home/steve/Desktop/Othello/src/Coordinates.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Coordinates.cpp.o"
-  "/home/steve/Desktop/Othello/src/Game.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Game.cpp.o"
-  "/home/steve/Desktop/Othello/src/GameLogic.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/GameLogic.cpp.o"
-  "/home/steve/Desktop/Othello/src/HumanPlayer.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/HumanPlayer.cpp.o"
-  "/home/steve/Desktop/Othello/src/Player.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Player.cpp.o"
-  "/home/steve/Desktop/Othello/src/main.cpp" "/home/steve/Desktop/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/main.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/AIplayer.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/AIplayer.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/BasicRules.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/BasicRules.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/Board.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Board.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/CharBoard.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/CharBoard.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/Coordinates.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Coordinates.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/Game.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Game.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/GameLogic.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/GameLogic.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/HumanPlayer.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/HumanPlayer.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/Player.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/Player.cpp.o"
+  "/home/avshalom/CLionProjects/Othello/src/main.cpp" "/home/avshalom/CLionProjects/Othello/cmake-build-debug/CMakeFiles/Othello.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
