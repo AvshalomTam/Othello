e44be8b83c168d7905fa324f341e676f637a1736
SimSteve <simgutfreund@gmail.com>
2017-11-29 23:36:21 +0200

    fixed

--- test/AIplayerTest.h
+++ test/AIplayerTest.h
@@ -7,36 +7,33 @@
 #include "../include/CharBoard.h"
 #include "../include/BasicRules.h"
 #include "../include/AIplayer.h"
+#include "../include/ConsoleGameFlow.h"
 
 /**
  * Testing AIplayer.
  */
-/*
-
 class AIplayerTest : public testing::Test {
  public:
-  AIplayerTest(): board_(), judge_(), pl_(AIplayer(board_, judge_, second_player)) {}
+  AIplayerTest(): board_(), judge_(), gameflow_(),  pl_(AIplayer(board_, judge_, gameflow_, second_player)) {}
 
   virtual void SetUp() {
     Coordinates move = Coordinates(2, 3);
     judge_.turnTiles(board_, move, first_player); //actually puts in cell 3,4
   }
-  */
-/**
-  * making another 2 moves in the game by turning tiles in the board.
-  *//*
 
+  /**
+  * making another 2 moves in the game by turning tiles in the board.
+  */
   void setUp2() {
     Coordinates move = Coordinates(2, 2);
     judge_.turnTiles(board_, move, second_player); //actually puts in cell 3,3
     move = Coordinates(2, 1);
     judge_.turnTiles(board_, move, first_player); //actually puts in cell 3,2
   }
-  */
-/**
-  * making another 2 moves in the game by turning tiles in the board.
-  *//*
 
+  /**
+  * making another 2 moves in the game by turning tiles in the board.
+  */
   void setUp3() {
     Coordinates move = Coordinates(1, 1);
     judge_.turnTiles(board_, move, second_player); //actually puts in cell 2,2
@@ -47,6 +44,7 @@ class AIplayerTest : public testing::Test {
   CharBoard board_;
   BasicRules judge_;
   AIplayer pl_;
+  ConsoleGameFlow gameflow_;
 };
 
 //testing getMove function several times
@@ -57,6 +55,6 @@ TEST_F(AIplayerTest, minmax_calculate) {
   setUp3();
   EXPECT_EQ(pl_.getMove().toString(), "(0,0)");
 }
-*/
+
 
 #endif //AIPLAYERTEST_H
